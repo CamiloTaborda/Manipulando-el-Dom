2d04ab4dc1e4ee9bcb2766851bd27aa9f56cef86
Camilo Taborda <camilotaborda2010@gmail.com>
2023-06-02 09:05:36 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const h1 = document.querySelector("h1");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+const pResult = document.querySelector("#resultado");
+
+btn.addEventListener("click", sumaInputs)
+
+function sumaInputs() {
+   const result = `${input1.value} ${input2.value}` 
+   pResult.innerText = result
+}
